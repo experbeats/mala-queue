66010591cf0dd4c593422affcca9b74ee3748351
Byron Montiel <71675989+experbeats@users.noreply.github.com>
2022-05-19 11:05:07 -0400

    Update run.js

--- code/src/run.js
+++ code/src/run.js
@@ -5,8 +5,8 @@
   {
     // Constantes
     const VERSION = 4;
-    const EVENTO = 'biz175';
-    const NUM_FECHA = 2;
+    const EVENTO = 'biz177';
+    const NUM_FECHA = 1;
     const TIPO_TICKET_ID_MIN = 10;
     const TIPO_TICKET_ID_MAX = 12;
     const BASE_FETCH_PARAMS = {
